5e9c59454786d0e1294c77d62c9635fdf8d2c931
Asep rayana <asep.rayana09@gmail.com>
2024-10-16 19:22:55 +0700

    generate

--- s/password.go
+++ s/password.go
@@ -1,6 +1,10 @@
 package util
 
 import (
+	"math/rand"
+	"strings"
+	"time"
+
 	dto "github.com/srv-cashpay/auth/dto/auth"
 
 	"golang.org/x/crypto/bcrypt"
@@ -14,6 +18,14 @@ func VerifyPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
 
+func GenerateFromWhatsapp(password string) ([]byte, error) {
+	return bcrypt.GenerateFromWhatsapp([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyWhatsapp(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
 func EncryptPassword(req *dto.SignupRequest) (err error) {
 	hashedPassword, err := GenerateFromPassword(req.Whatsapp)
 
@@ -22,4 +34,17 @@ func EncryptPassword(req *dto.SignupRequest) (err error) {
 	}
 	req.Whatsapp = string(hashedPassword)
 	return nil
+}
+
+func GenerateRandomNumeric(length int) string {
+	const chars = "0123456789"
+
+	var result strings.Builder
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < length; i++ {
+		result.WriteRune(rune(chars[rand.Intn(len(chars))]))
+	}
+
+	return result.String()
 }
